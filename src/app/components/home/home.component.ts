7a76a0dbd417e48f2107088e22afeaf375e042e0
IGOR ANDRADE SOARES <igsoares89@gmail.com>
2026-06-17 11:39:49 -0300

    commit automatico

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
