7c129fc2fbeb67bedfcf4a214ab0cb00969b5064
IGOR ANDRADE SOARES <igsoares89@gmail.com>
2026-06-25 11:07:52 -0300

    commit automatico

--- src/app/components/tabela-carros/tabela-carros.component.ts
+++ src/app/components/tabela-carros/tabela-carros.component.ts
@@ -10,7 +10,7 @@ import { DashboardService } from '../../services/dashboard.service';
 })
 export class TabelaCarrosComponent {
 
-    @Input() veiculo_data: VeiculoData = {
+    veiculo_data: VeiculoData = {
         id:-1,
         odometro:0,
         nivelCombustivel:0,
@@ -24,8 +24,13 @@ export class TabelaCarrosComponent {
     buscarInfo(event: Event){
       const vin = event.target as HTMLInputElement;
       this.dshservice.getVinInfo(vin.value).subscribe({
-        error: (erro: any) => {
-          console.log(erro.message);
+        error: () => {
+          this.veiculo_data.id=-1
+          this.veiculo_data.odometro=0
+          this.veiculo_data.lat=0
+          this.veiculo_data.long=0
+          this.veiculo_data.nivelCombustivel=0
+          this.veiculo_data.status=''
         },
         next: (vdata: VeiculoData) => {
           this.veiculo_data = vdata
